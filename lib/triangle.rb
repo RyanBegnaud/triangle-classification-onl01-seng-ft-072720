af5088be328c46d2b2d1d3e75e9de26b8c0d9152
Ryan Begnaud <ryanbegnaud13@gmail.com>
2020-08-11 22:35:34 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,6 +11,9 @@ class Triangle
   def kind 
     if @length1 <= 0 || @length2 <= 0 || @length3 <= 0 
       raise TriangleError
+    elsif @length1 + @length2 !> @length3
+      raise TriangleError
+    
     end
     
     if @length1 == @length2 && @length2 == @length3
