03d530cd295e6af8dcc6c813150277c7d6f23189
Ryan Begnaud <ryanbegnaud13@gmail.com>
2020-08-11 22:34:44 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,6 +12,7 @@ class Triangle
     if @length1 <= 0 || @length2 <= 0 || @length3 <= 0 
       raise TriangleError
     end
+    
     if @length1 == @length2 && @length2 == @length3
       :equilateral
     elsif @length2 == @length3
